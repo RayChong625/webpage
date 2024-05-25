f4527a17d75a0ae89a60a77b68f461d01c7a80ca
RayChong625 <158522796+RayChong625@users.noreply.github.com>
2024-05-25 03:34:52 -0400

    info+pricing section commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+
+function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('active');
+}
