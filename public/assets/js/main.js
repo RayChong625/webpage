9ca9181aa4b0ddbe33c9d6390f10a256a7dee624
RayChong625 <158522796+RayChong625@users.noreply.github.com>
2024-05-26 22:09:20 -0400

    footer commit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,7 +3,22 @@ function toggleMenu() {
     navLinks.classList.toggle('active');
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    function toggleFooter() {
+        const footerGrid = document.querySelector('.footer-grid');
+        const footerResponsive = document.querySelector('.footer-responsive');
 
-function toggleMenu() {
-    document.querySelector('.nav-links').classList.toggle('active');
-}
+        if (window.innerWidth <= 800) {
+            footerGrid.style.display = 'none';
+            footerResponsive.style.display = 'block';
+        } else {
+            footerGrid.style.display = 'grid';
+            footerResponsive.style.display = 'none';
+        }
+    }
+
+
+    toggleFooter();
+
+    window.addEventListener('resize', toggleFooter);
+});
